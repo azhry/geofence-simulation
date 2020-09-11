f762b70b2a8fa648b3082065f57360ef28db4311
azhry <arliansyah_azhary@yahoo.com>
2020-09-11 10:58:56 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import geopandas as gpd
+import plotly_express as px
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('data/go_track_trackspoints_edited.csv')
+gdf = gpd.GeoDataFrame( df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
+px.set_mapbox_access_token('pk.eyJ1Ijoic2hha2Fzb20iLCJhIjoiY2plMWg1NGFpMXZ5NjJxbjhlM2ttN3AwbiJ9.RtGYHmreKiyBfHuElgYq_w')
+
+polygon = gpd.read_file('data/CENTERAREA.geojson')
+mask = (polygon.loc[0, 'geometry'])
+pip_mask_geofence = gdf.within(mask)
+
+gdf.loc[:,'geofence'] = pip_mask_geofence
+gdf['geofence'] = gdf['geofence'].replace({True: 'In', False: 'Out'})
+fig = px.scatter_mapbox(gdf, 
+                lat='latitude', lon='longitude', 
+                color='track_id', 
+                size='track_id', 
+                animation_frame='time', 
+                animation_group='track_id',
+                size_max=10, 
+                zoom=12, 
+                # text='time',
+                width=1200, 
+                height=800)
+
+fig.show()
